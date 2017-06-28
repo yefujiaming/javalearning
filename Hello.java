49a5fe746dbd86eb03710f682a6466136d717959
傅家鸣 <jamesfu_vip@163.com>
2017-06-28 18:06:00 +0800

    helloword

---
+++ Hello.java
@@ -0,0 +1,16 @@
+//作者：傅家鸣
+//功能：在控制台显示“hello”
+//日期：2017.6.28
+
+//public：表示这个类是公共的，一个java文件中只能有一个public
+//class：表示这是一个类
+//hello:类名（公共类类名必须和文件名一致）
+
+public class Hello
+{
+	//一个主函数，相当于程序的入口
+    public static void main(String[] args) {
+    	//执行语句
+		System.out.println("hello!");
+	}
+}
