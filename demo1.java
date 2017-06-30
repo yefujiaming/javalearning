b4dfcf467facde0980bbf9f01133655053915616
傅家鸣 <jamesfu_vip@163.com>
2017-06-30 18:24:31 +0800

    demo1

---
+++ demo1.java
@@ -0,0 +1,19 @@
+/*
+  作者：傅家鸣
+  日期：2017.6.30
+  功能：判断两个数是否能整除
+  */
+  public class demo1
+  {
+  	public static void main(String[] args) 
+  	{
+  		int a=2;
+  		int b=4;
+  		if(b%a==0)
+  		{
+  			System.out.println("可以整除");
+  		}else{
+  					System.out.println("不可以整除！");
+  		}
+  	}
+  }
